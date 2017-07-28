be58913b221033568d9653521f978602cb354451
Adrian Llerena <a.llerena001@gmail.com>
2017-07-28 04:10:46 -0400

    predone

--- bin/seed.js
+++ bin/seed.js
@@ -6,7 +6,7 @@
 const mongoose = require('mongoose');
                                     // database name
                                     //    |
-  mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
                                //         |
                                // use ironshop
 
